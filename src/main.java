f83866729ced0d43d6796c7aee33505ba1f75f5c
Mahsum OZER <46961602+mahsumozer@users.noreply.github.com>
2021-06-16 02:38:26 +0300

    fucking 4

--- src/main.java
+++ src/main.java
@@ -1,6 +1,25 @@
 public class main {
+    ///english class <><><><><>
     public static void main(String[] args) {
-
+    dortislem(10,20,'+');
     }
+    // 3 prameteres , 2 number , 1 operator
+//ex :10 20
+    //0.5
+
+    public static void dortislem (double a,double b,char c){
+        if (c=='/'){
+            System.out.println(a/b);
+        }else if(c=='*'){
+            System.out.println(a*b);
+        }else if(c=='+'){
+            System.out.println(a+b);
+        }else if(c=='-'){
+            System.out.println(a-b);
+        }
+    }
+
+
+
 }
 //dd
