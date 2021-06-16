98c83fe8218975e20e07bedaecd6cc48eddbc5f4
Mahsum OZER <46961602+mahsumozer@users.noreply.github.com>
2021-06-17 01:44:43 +0300

    i add hey

--- src/exams.java
+++ src/exams.java
@@ -1,29 +1,30 @@
 public class exams {
+
+    public static int[] islem(int[] dizi){
+         int a=0;
+        for (int i=0;i<dizi.length ;i++){
+            for (int j=0 ;j<dizi.length-1;j++){
+                if (dizi[j]>dizi[j+1]){
+                    a=dizi[j+1];
+                    dizi[j+1]=dizi[j];
+                    dizi[j]=a;
+                }
+            }
+        }
+
+        return dizi;
+    }
+
     public static void main(String[] args) {
-        System.out.println(add(1,1));
 
-
-        int[] dizi= {45,2,78,23,9,78,1};
-
-int a=dizi(21);
-        System.out.println("a="+a);
-
-        String aa="mahsum ";
-        String b="ozer";
-        System.out.println("concat ="+aa.concat(b));
-
+        int[] dizi= {45,2,78,23,9,79,1};
+        int[] dizi2=new int[dizi.length];
+        dizi2=islem(dizi);
+for (int i=0;i<dizi.length;i++){
+    System.out.println(dizi2[i]);
+}
     }
-    public static int dizi(int dizi){
-if (dizi==0){
 
-    return dizi;
-}System.out.println(dizi);
-        return dizi(dizi-1);
-    }
-///
 
-//
-    public static int add(int a, int b){
-        return a+b;
-    }
+
 }
